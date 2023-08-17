d7f11fb941f632f46dea4444db07a60d62cf72b7
Tyren <tyren.nawak@gmail.com>
2023-08-17 16:10:47 +0200

    Delete node_modules directory

--- node_modules/gridjs/plugins/selection/dist/src/rowSelection/actions.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const CheckRow: (rowId: string) => (state: any) => any;
-export declare const UncheckRow: (rowId: string) => (state: any) => any;
--- node_modules/gridjs/plugins/selection/dist/src/rowSelection/rowSelection.d.ts
+++
@@ -1,9 +0,0 @@
-import { h } from 'preact';
-import { Row } from 'gridjs';
-import { Cell } from 'gridjs';
-interface RowSelectionProps {
-    row?: Row;
-    cell?: Cell;
-}
-export declare function RowSelection(props: RowSelectionProps): h.JSX.Element;
-export {};
